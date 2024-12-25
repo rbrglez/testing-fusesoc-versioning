5c0aec2b96d4038745f52bc040c2acec2469c1c3
Rene Brglez <rene.brglez@aviatnet.com>
2024-12-25 14:21:14 +0100

    added/updated fusesoc initialization of local development branches

--- init_development_olo_4.0.0.sh
+++ init_development_olo_4.0.0.sh
@@ -1,8 +1,8 @@
 set -e
-name="open-logic-3.2.0"
+name="open-logic-4.0.0"
 location="development-cores/$name"
 rm -rf $location
 fusesoc library add $name git@github.com:rbrglez/open-logic.git --location $location
 
 cd $location
-git checkout test/3.2.0
+git checkout test/4.0.0
